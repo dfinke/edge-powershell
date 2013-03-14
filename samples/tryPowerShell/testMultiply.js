f44c67e4c954af863da9cf61e2717a7aff1637b8
Doug Finke <finked@hotmail.com>
2013-03-13 20:06:37 -0400

    Operate on output from PS

---
+++ samples/tryPowerShell/testMultiply.js
@@ -0,0 +1,11 @@
+var owin = require('../../lib/owin-powershell.js')
+
+var script = "1..5";
+
+powerShell(script, function (error, result) {
+    if (error) throw error;
+
+    result.forEach(function(item) {
+		console.log(item*2);
+	});
+});
