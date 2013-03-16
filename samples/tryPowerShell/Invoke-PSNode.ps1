2bdd7ea37042649786ad4ac1a2d9d8d53cdc4926
Doug Finke <finked@hotmail.com>
2013-03-16 17:02:20 -0400

    Added check for env var

--- samples/tryPowerShell/Invoke-PSNode.ps1
+++ samples/tryPowerShell/Invoke-PSNode.ps1
@@ -1,5 +1,9 @@
 function Invoke-PSNode {
     param($script)
 
+    if(!$env:OWIN_POWERSHELL_NATIVE) {
+        $env:OWIN_POWERSHELL_NATIVE = '..\..\src\Owin.PowerShell\Owin.PowerShell\bin\Debug\Owin.PowerShell.dll'
+    }
+
     ((node .\test.js $script) -join '' | ConvertFrom-Json ).result
 }
