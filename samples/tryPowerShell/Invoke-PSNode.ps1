d839399ce2fa78c6db3a0012bb1a0966b3a521a2
Doug Finke <finked@hotmail.com>
2013-03-16 19:20:10 -0400

    Updating to use Edge

--- samples/tryPowerShell/Invoke-PSNode.ps1
+++ samples/tryPowerShell/Invoke-PSNode.ps1
@@ -1,8 +1,8 @@
 function Invoke-PSNode {
     param($script)
 
-    if(!$env:OWIN_POWERSHELL_NATIVE) {
-        $env:OWIN_POWERSHELL_NATIVE = '..\..\src\Owin.PowerShell\Owin.PowerShell\bin\Debug\Owin.PowerShell.dll'
+    if(!$env:EDGE_POWERSHELL_NATIVE) {
+        $env:EDGE_POWERSHELL_NATIVE = '..\..\src\Owin.PowerShell\Owin.PowerShell\bin\Debug\Owin.PowerShell.dll'
     }
 
     ((node .\test.js $script) -join '' | ConvertFrom-Json ).result
